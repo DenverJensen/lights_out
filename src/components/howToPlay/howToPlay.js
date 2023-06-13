9c81dcc1585d7a6869eaf1bbdc5b972e4cacf7e4
DenverJensen <jensendenver@gmail.com>
2023-06-13 00:42:09 -0600

    how to play update

--- src/components/howToPlay/howToPlay.js
+++ src/components/howToPlay/howToPlay.js
@@ -3,7 +3,6 @@ import { Heading } from '@chakra-ui/react';
 const HowToPlay = () => {
   return (
     <>
-      {' '}
       <Heading size="sm" mt={8}>
         Object: <br />
         To turn all the lights out on the keypad, but each time you press a lit
@@ -11,17 +10,9 @@ const HowToPlay = () => {
         buttons! You must try to turn the lights out in the minimum number of
         moves! <br />
         <br />
-        Game: Lights Out contains three different game play modes, but for
-        purposes of the Quick Start play, we will begin with Mode 1, Puzzle 1!
-        Your game contains an On/Off button, a Start button, a Sound Button, a
-        Help Button and a Select Button. <br />
-        <br /> How To Play: Press the “On” Button - This will turn your game on.
-        You will see a “show of lights” chase around the board, ending in a
-        cross pattern. <br />
-        <br />
-        Press the “Start” Button - The first button in the top row (row 1) will
-        flash and then you will see three buttons in the middle row (row 3)
-        light up and stay lit. This is Mode 1, Puzzle 1. Now you must try to
+        Your game contains +/- buttons to switch the current level and a reset button.<br />
+        <br/>
+        You will start on Puzzle 1. Now you must try to
         turn these lights out!
       </Heading>
     </>
